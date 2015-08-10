c0816b2a9c48dbb651f94ecac49096a7097633a4
Pradeep Kiran Chakkirala <krnprdp@gmail.com>
2015-08-09 20:58:11 -0400

    add API Key holder class

---
+++ app/src/main/java/com/pradeep/nanodegree/popularmovies/tmdb.java
@@ -0,0 +1,23 @@
+package com.pradeep.nanodegree.popularmovies;
+
+/**
+ * Created by Pradeep on 8/9/15.
+ * <p/>
+ * Remember to add your tmdb API Key here. The Key is required for accessing themoviedb.org database from within the app.
+ * <p/>
+ * THE APP WILL CRASH WITHOUT A VALID API Key!!
+ * <p/>
+ * Due to copyright reasons, it is illegal to share API KEY publicly.
+ * <p/>
+ * You can generate your API KEY at https://www.themoviedb.org/documentation/api
+ */
+
+public class tmdb {
+
+    private static String api_key = ""; /* Place Your API Key here */
+
+    static String getKey() {
+        return api_key;
+    }
+
+}
