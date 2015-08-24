c7562afdb74289a1b18211641fdb2e288ab0ff74
Pradeep Kiran Chakkirala <krnprdp@gmail.com>
2015-08-23 21:41:27 -0400

    update movie details fragment - poopulate with data

--- app/src/main/java/com/pradeep/nanodegree/popularmovies/MovieDetailsActivityFragment.java
+++ app/src/main/java/com/pradeep/nanodegree/popularmovies/MovieDetailsActivityFragment.java
@@ -5,18 +5,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MovieDetailsActivityFragment extends Fragment {
 
+    String id = "", poster = "", title = "", synopsis = "", rating = "", release = "";
+
     public MovieDetailsActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        id = args.getString("id");
+        poster = args.getString("poster");
+        title = args.getString("title");
+        synopsis = args.getString("synopsis");
+        rating = args.getString("rating");
+        release = args.getString("release");
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movie_details, container, false);
+
+
+        View rootView = inflater.inflate(R.layout.fragment_movie_details, container, false);
+        TextView tvTitle = (TextView) rootView.findViewById(R.id.tvTitle);
+        TextView tvSynopsis = (TextView) rootView.findViewById(R.id.tvSynopsis);
+        TextView tvRelease = (TextView) rootView.findViewById(R.id.tvRelease);
+        TextView tvRating = (TextView) rootView.findViewById(R.id.tvRating);
+
+        tvTitle.setText(title);
+        tvRelease.setText("Release Date: " + release);
+        tvRating.setText("Rating: " + rating + "/10");
+        tvSynopsis.setText("Synopsis:\n" + synopsis);
+        ImageView ivPoster = (ImageView) rootView.findViewById(R.id.ivPoster);
+
+        Picasso.with(getActivity()).load(poster).into(ivPoster);
+
+
+        return rootView;
     }
 }
